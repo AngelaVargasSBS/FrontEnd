e94bf206dfcc8b54ddbed8bd7c263cdf7f554751
Angela.Vargas <luz.vargas@sbseguros.co>
2020-03-17 15:27:19 -0500

    reCommitSaveSumAssured

--- src/components/propertyForm/Step2.vue
+++ src/components/propertyForm/Step2.vue
@@ -544,7 +544,7 @@
                                       outlined
                                       @click="
                                         Guardarrisks();
-                                        stepFormSubmission()
+                                          stepFormSubmission()
                                       "
                                     >Continue</v-btn>
                                   </v-col>
@@ -880,8 +880,33 @@ export default {
       store
         .dispatch("riskPolicyStore/createRiskPolicy")
         .then(resp => {
-          store.commit("formPropertyStore/addNewRiskList");
+         
 
+         let arraySumms =  this.$store.state[
+      "productConfigurationStore"
+    ].productPlan.productPlanSummsAssured
+
+          arraySumms.forEach(element => { 
+            //if (element.summIncluded == true) {
+              store.dispatch("riskPolicyStore/createSummAssuredPolicy", element)
+                .then(resp => {
+                })
+                .catch(err => {
+                  this.$swal({
+                    position: "center",
+                    icon: "error",
+                    title: "Error: ",
+                    text: err,
+                    showConfirmButton: true
+                  });
+                });
+            //}
+          });
+
+          store.commit("formPropertyStore/addNewRiskList");
+          store.commit("formPropertyStore/resetState");
+          store.commit("productConfigurationStore/resetProductPlanSummsAssured")
+          this.dialogEdit =false
           this.$swal({
             position: "center",
             icon: "success",
@@ -902,11 +927,8 @@ export default {
           });
         });
     },
-    saveNewPropertyRiskPolicy() {
-      store.dispatch("riskPolicyStore/postPropertyRiskPolicy");
-    },
 
-    Guardarrisks() {
+    GuardarProteccion() {
       console.log(`paso 1 statusNewTariffRisk = ${this.statusNewTariffRisk}`);
       if (
         this.editedItemrisks.tipoAmparo != "" &&
@@ -1023,12 +1045,6 @@ export default {
                 showConfirmButton: false,
                 timer: 4000
               });
-              console.log("this.editedIndexrisks");
-              console.log(this.editedIndexrisks);
-              console.log("/this.editedIndexrisks");
-              store.dispatch("riskPolicyStore/postPropertyRiskPolicy", {
-                key: this.editedIndexrisks
-              });
             },
             error => {
               this.$swal({
@@ -1039,9 +1055,6 @@ export default {
                 showConfirmButton: false,
                 timer: 4000
               });
-              console.log("this.editedIndexrisks");
-              console.log(this.editedIndexrisks);
-              console.log("/this.editedIndexrisks");
             }
           );
       }
--- src/store/logicModules/propertyRiskPolicy/riskPolicyStore.js
+++ src/store/logicModules/propertyRiskPolicy/riskPolicyStore.js
@@ -1,14 +1,55 @@
 import Vue from 'vue'
 import restApi from '@/store/utils/restApi.js'
 
+const defaultState = {
+
+  riskSummsAssured: [],
+  riskCoverages: [],
+  riskNumber: null,
+  planCode: null,
+  sumInsuredLocalCurrency: null,
+  riskProperty: {
+    address: null,
+    complementaryAddress: null,
+    constructionYear: null,
+    countryCode: null,
+    departamentCode: null,
+    intersectionStreetTypeCode: null,
+    latitudeNumber: null,
+    townCode: null,
+    normalizedAddress: null,
+    numberLength: null,
+    numberOfFloors: null,
+    numberUnits: null,
+    postalCode: null,
+    propertyName: null,
+    streetTypeCode: null,
+    street1: null,
+    street1Letter: null,
+    street1Prefix: null,
+    street1LetterPrefix: null,
+    street1QuadrantCode: null,
+    intersectionStreetTypeCode: null,
+    street2: null,
+    street2Letter: null,
+    street2Prefix: null,
+    street2LetterPrefix: null,
+    street2QuadrantCode: null,
+    urban: true,
+  }
+
+}
+
+
+
+
 const getters = {
 
 }
 const actions = {
 
   createRiskPolicy({
-    commit,
-    state
+    commit, state
   }) {
 
 
@@ -51,44 +92,64 @@ const actions = {
     })
 
   },
+  createSummAssuredPolicy({
+    commit, state
+  }, dataSumm) {
 
-  postPropertyRiskPolicy(state, data) {
-    let dataRisk = state.rootState.formPropertyStore.step2.risks[data.key + 1];
+
+    let dataPost = {
+      uniqueIdentifier: this.state.formPropertyStore.step1.uniqueIdentifier,
+      anniversary: this.state.formPropertyStore.step1.anniversary,
+      riskNumber: state.riskNumber,
+      typeRiskCode: this.state.formPropertyStore.step1.typeRiskCode,
+      sumAssured: dataSumm.summAssured,
+      insuredSumCode: dataSumm.codigoSuma
+    }
+
+
+
+
     return new Promise((resolve, reject) => {
-      let url = Vue.prototype.$urlServices + `/api/v1/sbs/createPropertyRiskPolicy/propertyRiskPolicy`;
-      let dataPost = {
-        address: dataRisk.address,
-        anniversary: this.state.formPropertyStore.step1.anniversary,
-        uniqueIdentifier: this.state.formPropertyStore.step1.uniqueIdentifier,
-        ciuCode: null,
-        codeTypeRisk: this.state.formPropertyStore.step1.typeRiskCode,
-        riskNumber: dataRisk.riskNumber,
-        countryCode: dataRisk.countryCode,
-        departamentCode: dataRisk.departamentCode,
-        municipalityCode: dataRisk.municipalityCode,
-      }
+
+      let url = Vue.prototype.$urlServices + `/api/v1/oal/sumInsuredRisk`
 
       restApi.post(url, dataPost)
         .then(response => {
+
           if (response.data.status.code == 200) {
+
+            commit('setNewSummAssuredRisk', dataPost);
             resolve()
+
           } else {
+
             reject(response.data.status.message)
           }
+
         })
         .catch(e => {
+
           reject(e)
         })
-    })
-  },
-}
 
+    })
+
+  }
+}
 const mutations = {
   setNewRisk(state, dataNewRisk) {
 
     state.riskNumber = dataNewRisk.riskNumber
     state.planCode = dataNewRisk.planCode
 
+  },
+  setNewSummAssuredRisk(state, dataNewSumm) {
+
+    state.riskSummsAssured.push(dataNewSumm)
+
+  },
+  resetState(state){
+    Object.assign(state, defaultState)
   }
 
 }
